41eae548663ac0d985e70c7b2dd77bfef6cb7e1a
Jon Patterson <JonPatt92@gmail.com>
2020-01-30 14:24:41 -0700

    Build location conditional to provide different responses based on whether a location was provided

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -7,26 +7,33 @@ const database = require('knex')(configuration);
 
 router.get('/', (request, response) => {
   var api_key = request.query.api_key
-  var location = request.query.location
+  var location = request.query.location || null
+
   database('users').where('api_key', api_key).first()
     .then(user => {
       if (user === undefined) {
         return response.status(404).json({error: 'Invalid API key'})
       } else {
-        database('favorites')
-          .returning('location')
-          .insert({user_id: user.id, location: location})
-            .then(favorite_location => {
-              return response.status(200).json({message: `${favorite_location[0]} has been added to your favorites`})
-            })
-            .catch((error) => {
-              response.status(500).json({ error });
-            });
+        if (location) {
+          database('favorites')
+            .returning('location')
+            .insert({user_id: user.id, location: location})
+              .then(favorite_location => {
+                return response.status(200).json({message: `${favorite_location[0]} has been added to your favorites`})
+              })
+              .catch((error) => {
+                response.status(500).json({ error });
+              });
+        } else if (location === null) {
+          //return forecast for each location
+        };
       }
     })
     .catch((error) => {
       response.status(500).json({ error });
     });
 });
+
+
 
 module.exports = router;
